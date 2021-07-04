3a1ab1efae162b714987e1d893ebb32a35ac9bb9
sskim91 <tjdtjq91@gmail.com>
2021-07-04 19:25:52 +0900

    버퍼와 스트림

---
+++ ch03/createWriteStream.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+const writeStream = fs.createWriteStream('./writeme2.txt');
+writeStream.on('finish', () => {
+    console.log('파일 쓰기 완료');
+});
+
+writeStream.write('이 글을 씁니다.\n'); //이게 하나의 버퍼
+writeStream.write('한 번 더 씁니다.');
+writeStream.end();
